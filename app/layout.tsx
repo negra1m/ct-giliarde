56837778fdc366c9e20b7609e31b8a760d685897
Negrão <vnsn_@outlook.com>
2025-08-01 18:22:54 -0300

    final version

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'CT Giliarde de Lima',
+  description: 'A melhor academia de luta da região',
+  generator: 'fewcompany.com',
 }
 
 export default function RootLayout({
